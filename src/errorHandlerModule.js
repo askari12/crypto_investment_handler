85035c25af9ac105c6f7f2564ea8652f9aa9f73a
Askari Iqbal <askari.i@justsnap.de>
2022-03-12 16:13:19 +0500

    Added Error file name in the error handler

--- src/errorHandlerModule.js
+++ src/errorHandlerModule.js
@@ -1,7 +1,36 @@
 import chalk from 'chalk'
 
+function _getCallerFile() {
+  var originalFunc = Error.prepareStackTrace;
+
+  var callerfile;
+  try {
+      var err = new Error();
+      var currentfile;
+
+      Error.prepareStackTrace = function (err, stack) { return stack; };
+
+      currentfile = err.stack.shift().getFileName();
+
+      while (err.stack.length) {
+          callerfile = err.stack.shift().getFileName();
+
+          if(currentfile !== callerfile) break;
+      }
+  } catch (e) {}
+
+  Error.prepareStackTrace = originalFunc; 
+
+  return callerfile;
+}
+
 const errorHandler = (error) => {
-  console.log(`ERROR : ${chalk.bgRed(error.message)}`);
+  const callerFileFullPath = _getCallerFile();
+  const callerFile = callerFileFullPath.split('/').pop();
+  console.log(`${chalk.red("*****************************")}`)
+  console.log(`ERROR Caller File: ${chalk.bgRed(callerFile)}`);
+  console.log(`ERROR Message: ${chalk.bgRed(error.message)}`);
+  console.log(`${chalk.red("*****************************")}`)
 }
 
 export function handleError(e) {

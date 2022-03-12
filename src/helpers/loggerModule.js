de3e0dea665636122054b7d202e0c886027c928f
Askari Iqbal <askari.i@justsnap.de>
2022-03-12 16:28:07 +0500

    Logger Module updated to account for tags and no tag messages

--- src/helpers/loggerModule.js
+++ src/helpers/loggerModule.js
@@ -1,9 +1,16 @@
 import chalk from "chalk"
 
-const _logger = (message , tag = "Default Tag") => {
+const _loggerT = (message , tag) => {
   console.log(chalk.bgBlue("TAG"), chalk.blueBright(tag) , chalk.bgBlue("MESSAGE"), chalk.blueBright(message));
 }
 
+const _loggerM = (message) => {
+  console.log(chalk.bgBlue("MESSAGE"), chalk.blueBright(message));
+}
+
 export function logger(m , t) {
-  _logger(m, t)
+  if (t)
+    _loggerT(m, t)
+  else
+    _loggerM(m)
 }
